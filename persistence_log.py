234fe5afe36c5ff1b79124d2699b80cc04d988dc
joseph080508 <joseph.080508@gmail.com>
2026-03-27 22:04:35 -0500

    Implementando funcion de add_blocker
    Added a function to append daily blockers to a file.

--- persistence_log.py
+++ persistence_log.py
@@ -1,2 +1,12 @@
 import os 
 
+FILE_NAME = "database.txt"
+
+# CREATE Append data
+def add_blocker():
+    blocker = input("Enter your Daily Blocker: ")
+
+    with open(FILE_NAME, "a") as file:
+        file.write(blocker + "\n")
+
+    print("Blocker saved successfully.\n")
