c8459a734b274cf49797dbde64536818e8759293
joseph080508 <joseph.080508@gmail.com>
2026-03-27 22:12:22 -0500

    Implementando funcion fetch_blockers

--- persistence_log.py
+++ persistence_log.py
@@ -10,3 +10,21 @@ def add_blocker():
         file.write(blocker + "\n")
 
     print("Blocker saved successfully.\n")
+
+# READ Fetch data
+def fetch_blockers():
+    if not os.path.exists(FILE_NAME):
+        print("Warning: The file does not exist yet.\n")
+        return
+
+    with open(FILE_NAME, "r") as file:
+        blockers = file.readlines()
+
+    if len(blockers) == 0:
+        print("No blockers found.\n")
+    else:
+        print("\n--- Team Daily Blockers ---")
+        for i, blocker in enumerate(blockers, start=1):
+            print(f"{i}. {blocker.strip()}")
+        print()
+
